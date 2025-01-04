9f4d80044ae5efd894413945441da07619ea199a
wamweaa <patokinya12@gmail.com>
2025-01-04 20:03:36 +0300

    blog section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import HomePage from './components/HomePage'
 import ContactPage from './components/ContactPage'
+import BlogSection from './components/BlogSection'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
     <HomePage/>
     <ContactPage/>
+    <BlogSection/>
     </>
   )
 }
